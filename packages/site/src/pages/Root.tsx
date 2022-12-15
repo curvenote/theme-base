07f56ef6ce31403f45f469214e80d4bdca40311e
Rowan Cockett <rowanc1@gmail.com>
2022-12-15 10:58:04 -0700

    📜 Improved content reload with scroll

--- packages/site/src/pages/Root.tsx
+++ packages/site/src/pages/Root.tsx
@@ -48,7 +48,7 @@ export function Document({
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
-        <ContentReload port={CONTENT_CDN_PORT} />
+        {process.env.NODE_ENV !== 'development' ? null : <ContentReload port={CONTENT_CDN_PORT} />}
       </body>
     </html>
   );
